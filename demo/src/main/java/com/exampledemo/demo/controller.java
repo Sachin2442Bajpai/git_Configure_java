7d82e4f3ca324a8e5ee46b9b404db2162206e905
Sachin2442Bajpai <134322852+Sachin2442Bajpai@users.noreply.github.com>
2024-08-28 16:34:49 +0000

    commiyt all file in Spring Boot

---
+++ demo/src/main/java/com/exampledemo/demo/controller.java
@@ -0,0 +1,13 @@
+package com.exampledemo.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @g
+    public String home()
+    {
+       return "hello sachin bajpai";
+    }
+
+}
